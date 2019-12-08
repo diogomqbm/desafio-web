f909888e12c6f148cfe64aec77d816d8209f667b
Diogo <diogomqbm13@gmail.com>
2019-12-08 20:49:15 -0200

    creating repoPulls route

---
+++ src/modules/RepoPulls/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const RepoPulls = ({ history }) => {
+
+  return (
+    <div>
+      bla
+    </div>
+  );
+}
+
+export default RepoPulls;
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,15 @@
 import Search from "./modules/Search";
+import RepoPulls from "./modules/RepoPulls";
 
 const routes = [
   {
     path: "/",
     exact: true,
     component: Search
+  },
+  {
+    path: "/:creator/:repoName",
+    component: RepoPulls
   }
 ]
 
